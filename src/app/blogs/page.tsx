63b21a7ed4ce421b4650c9d37896b20eea29d68d
Rayhan-abdullha <captainrayhan215@gmail.com>
2026-03-01 21:04:36 +0600

    fix: issues

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -3,7 +3,7 @@ import BlogListClient from './components/BlogListClient';
 
 export default async function BlogListPage() {
   const posts = await getAllPosts();
-  await new Promise((resolve) => setTimeout(resolve, 500));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
   return <BlogListClient posts={posts} />;
 }
